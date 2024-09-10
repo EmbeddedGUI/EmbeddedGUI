f6cc848adfa3b1ed07f0db1e45982aae06bec117
bob.wen <wenbo13579@163.com>
2024-09-10 23:04:01 +0800

    移除默认配置参数，跟随全局配置

--- example/HelloPerformace/app_egui_config.h
+++ example/HelloPerformace/app_egui_config.h
@@ -5,21 +5,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define EGUI_CONFIG_SCEEN_WIDTH  240
-#define EGUI_CONFIG_SCEEN_HEIGHT 320
-#define EGUI_CONFIG_COLOR_DEPTH  16
-
-#define EGUI_CONFIG_PFB_WIDTH  24
-#define EGUI_CONFIG_PFB_HEIGHT 32
-
-// debug options
-#define EGUI_CONFIG_DEBUG_PFB_REFRESH          0
-#define EGUI_CONFIG_DEBUG_DIRTY_REGION_REFRESH 0
-#define EGUI_CONFIG_DEBUG_INFO_SHOW            0
-
-#define EGUI_CONFIG_FONT_DEFAULT &egui_res_font_montserrat_18_4
-
 #define EGUI_CONFIG_DEBUG_FORCE_REFRESH_ALL 1
 
 #define EGUI_CONFIG_MAX_FPS 1
