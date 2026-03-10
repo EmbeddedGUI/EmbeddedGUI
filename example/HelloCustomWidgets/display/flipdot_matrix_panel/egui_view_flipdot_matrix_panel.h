615395240dd5d8c4c0b5ca30caa7da246340cdf6
wenbo <wenbo13579@163.com>
2026-03-10 15:35:32 +0800

    feat: add flipdot_matrix_panel custom widget

---
+++ example/HelloCustomWidgets/display/flipdot_matrix_panel/egui_view_flipdot_matrix_panel.h
@@ -0,0 +1,45 @@
+#ifndef _EGUI_VIEW_FLIPDOT_MATRIX_PANEL_H_
+#define _EGUI_VIEW_FLIPDOT_MATRIX_PANEL_H_
+
+#include "egui.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EGUI_VIEW_FLIPDOT_MATRIX_PANEL_MAX_STATES 8
+
+typedef struct egui_view_flipdot_matrix_panel_state egui_view_flipdot_matrix_panel_state_t;
+struct egui_view_flipdot_matrix_panel_state
+{
+    const char *preset;
+    const char *status;
+    const char *summary;
+    const char *footer;
+    uint8_t focus_row;
+    uint8_t accent_mode;
+};
+
+typedef struct egui_view_flipdot_matrix_panel egui_view_flipdot_matrix_panel_t;
+struct egui_view_flipdot_matrix_panel
+{
+    egui_view_t base;
+    const egui_view_flipdot_matrix_panel_state_t *states;
+    const egui_font_t *font;
+    uint8_t state_count;
+    uint8_t current_index;
+    uint8_t last_zone;
+};
+
+void egui_view_flipdot_matrix_panel_set_states(
+        egui_view_t *self,
+        const egui_view_flipdot_matrix_panel_state_t *states,
+        uint8_t state_count);
+void egui_view_flipdot_matrix_panel_set_font(egui_view_t *self, const egui_font_t *font);
+void egui_view_flipdot_matrix_panel_init(egui_view_t *self);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
